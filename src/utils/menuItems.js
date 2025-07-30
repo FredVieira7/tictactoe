62dea5cc45d1566bdde7f2532902c5f5293798e8
fredvieira <fredvieira0709@gmail.com>
2025-07-30 16:22:33 -0300

    feat: criando utils para menuItems

---
+++ src/utils/menuItems.js
@@ -0,0 +1,6 @@
+export const menuItems = [
+    { label: 'Cor do X (Jogador 1)', key: 'x' },
+    { label: 'Cor do O (Jogador 2)', key: 'o' },
+    { label: 'Cor dos Textos', key: 'text' },
+    { label: 'Cor do Tabuleiro', key: 'board' },
+]
